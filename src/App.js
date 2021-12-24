9e05086675ac90f4b19b130ad8d0e9302a5c2726
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-12-24 12:18:08 +0200

    added scss folders, components pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
